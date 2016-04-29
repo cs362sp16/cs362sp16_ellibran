83dbb7a43dadb8619689160228a5dc10eeeb3602
Dioxu <ellis113@gmail.com>
2016-04-28 22:17:18 -0700

    Trying out the first Cardtest

---
+++ dominion/cardtest1.c
@@ -0,0 +1,36 @@
+////////////////////////
+//smithy Unit Test
+////////////////////////
+
+#include "dominion.h"
+#include <stdio.h>
+
+int failed = 0;
+
+int myassert(int b,char* msg) {
+  if (b == 0) {
+    printf("smithy Test Failure: %s\n",msg);
+    failed = 1;
+  }
+}
+
+void checkasserts() {
+  if (!failed) {
+    printf ("smithy Successful\n");
+  }
+}
+
+int main()
+{
+	struct gameState g;
+	int r;
+	
+	int k[10] = {smithy,adventurer,gardens,embargo,cutpurse,mine,ambassador,outpost,baron,tribute};
+
+	initializeGame(2, k, 5, &g);
+	
+	r = cardEffect(7, 1, 1, 1, &s, 3, 0);
+	myassert(r == 0, "buyCard, should return valid 0");
+
+	checkasserts();
+}
